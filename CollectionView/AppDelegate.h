d0febe82d398b8e779032d4ce7a2508fac20950d
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-11-18 15:03:27 +0530

    Initial Commit

---
+++ CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by Bsetecip10 on 18/11/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
